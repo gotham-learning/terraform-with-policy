81ec257fda82acccf6abd06eecdb384dcd6ddb02
Wasin Watthanasrisong <wasinwatt@gmail.com>
2019-06-25 11:56:54 +0700

    add self-service permission to user

--- main.tf
+++ main.tf
@@ -8,4 +8,33 @@ resource "aws_iam_user" "nopporn" {
   tags = {
     full-name = "Nopporn Chumnininini"
   }
+
+  force_destroy = true
+}
+
+// Self-service policy
+resource "aws_iam_user_policy_attachment" "self-service-atm" {
+  user = aws_iam_user.nopporn.name
+  policy_arn = aws_iam_policy.self-service-policy.arn
+}
+
+resource "aws_iam_policy" "self-service-policy" {
+  name = "SelfService"
+  policy = data.aws_iam_policy_document.self-service-doc.json
+}
+
+data "aws_iam_policy_document" "self-service-doc" {
+  statement {
+    sid       = "AllowIndividualUserToSeeAndManageOnlyTheirOwnAccountInformation"
+    effect    = "Allow"
+    resources = ["arn:aws:iam::*:user/$${aws:username}"]
+
+    actions = [
+      "iam:ChangePassword",
+      "iam:CreateAccessKey",
+      "iam:DeleteAccessKey",
+      "iam:ListAccessKeys",
+      "iam:UpdateAccessKey",
+    ]
+  }
 }
