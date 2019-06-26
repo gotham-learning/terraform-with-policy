4c29f8436ac95133dd92e03405b790333ca270eb
Wasin Watthanasrisong <wasinwatt@gmail.com>
2019-06-26 13:24:35 +0700

    add ec2 & s3 permission

--- main.tf
+++ main.tf
@@ -38,3 +38,15 @@ data "aws_iam_policy_document" "self-service-doc" {
     ]
   }
 }
+
+// Deploy EC2 instance
+resource "aws_iam_user_policy_attachment" "ec2-full-access" {
+  user = aws_iam_user.nopporn.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+// Deploy S3 instance
+resource "aws_iam_user_policy_attachment" "s3-full-access" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  user = aws_iam_user.nopporn.name
+}
